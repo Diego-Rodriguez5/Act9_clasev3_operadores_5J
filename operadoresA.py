b48c0c6cfaf283362b188c5f838912ab6e0aa6df
Diego-Rodriguez5 <a.22308051281296@cbtis128.edu.mx>
2024-09-20 09:36:06 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,52 @@
+print("\n Act 9 clases v2")
+print("Diego Rodriguez: 22308051281296 \n")
+# Zona de class
+
+class Operadores1296:
+    def suma1296(self,E,N):
+        s1296=E+N
+        print(f"La suma de {E} + {N} = {s1296}\n")     
+    def resta1296(self,F,G):
+        o1296=F-G
+        print(f"La resta de {F} - {G} = {o1296}\n")
+    def multiplicacion1296(self,H,I):
+        p1296=H*I
+        print(f"La multiplicacion de {H} * {I} = {p1296}\n")
+    def division1296(self,J,K):
+        q1296=J/K
+        print(f"La division de {J} / {K} = {q1296}\n")
+    def modulo1296(self,L,M):
+        r1296=L%M
+        print(f"El modulo de {L} % {M} = {r1296}\n")
+    def exponente1296(self,N,O):
+        s1296=N**O
+        print(f"El esponente de {N} ** {O} = {s1296}\n")
+    def cociente1296(self,P,Q):
+        t1296=P//Q
+        print(f"El cociente de {P} // {Q} = {t1296}\n")
+
+        
+# Zona de creacion del objeto
+operadiego=Operadores1296()
+
+# Zona de uso de objeto
+print(" - Funcion suma")
+operadiego.suma1296(5,4)
+
+print(" - Funcion resta")
+operadiego.resta1296(10,5)
+
+print(" - Funcion multiplicacion")
+operadiego.multiplicacion1296(5,10)
+
+print(" - Funcion division")
+operadiego.division1296(2,10)
+
+print(" - Funcion modulo")
+operadiego.modulo1296(2,10)
+
+print(" - Funcion exponente")
+operadiego.exponente1296(3,2)
+
+print(" - Funcion cociente")
+operadiego.cociente1296(2,2)
